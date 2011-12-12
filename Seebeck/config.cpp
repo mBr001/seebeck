ee6f3cdc5d226b5970a3a0c4bb480974b1f5dcf6
Jiri Pinkava <jiri.pinkava@vscht.cz>
2011-12-12 21:45:36 +0100

    add configuration saving for sample

--- Seebeck/config.cpp
+++ Seebeck/config.cpp
@@ -6,6 +6,15 @@ const char Config::str_eurotherm_slave[] = "eurother slave";
 const char Config::str_hp34970_port[] = "hp34970 port";
 const char Config::str_msdp_port[] = "msdp port";
 const char Config::str_ps6220_port[] = "ps 6220 port";
+const char Config::str_sample_l1[] = "sample l1";
+const char Config::str_sample_l2[] = "sample l2";
+const char Config::str_sample_l3[] = "sample l3";
+const char Config::str_sample_S[] = "sample S";
+const char Config::str_sample_h[] = "sample height";
+const char Config::str_sample_w[] = "sample width";
+const char Config::str_resistivity_i[] = "resistivity i";
+const char Config::str_experimentator[] = "experimentator";
+
 
 Config::Config()
 {
@@ -26,6 +35,11 @@ int Config::eurothermSlave()
     return settings.value(str_eurotherm_slave).toInt();
 }
 
+QString Config::experimentator()
+{
+    return settings.value(str_experimentator).toString();
+}
+
 QString Config::hp34970Port()
 {
     return settings.value(str_hp34970_port).toString();
@@ -39,6 +53,41 @@ QString Config::msdpPort()
 QString Config::ps6220Port()
 {
     return settings.value(str_ps6220_port).toString();
+}
+
+double Config::resistivity_I()
+{
+    return settings.value(str_resistivity_i).toDouble();
+}
+
+double Config::sample_h()
+{
+    return settings.value(str_sample_h).toDouble();
+}
+
+double Config::sample_1()
+{
+    return settings.value(str_sample_l1).toDouble();
+}
+
+double Config::sample_l2()
+{
+    return settings.value(str_sample_l2).toDouble();
+}
+
+double Config::sample_l3()
+{
+    return settings.value(str_sample_l3).toDouble();
+}
+
+double Config::sample_S()
+{
+    return settings.value(str_sample_S).toDouble();
+}
+
+double Config::sample_w()
+{
+    return settings.value(str_sample_w).toDouble();
 }
 
 void Config::setDataDir(const QString &dirName)
@@ -56,6 +105,11 @@ void Config::setEurothermSlave(int slave)
     settings.setValue(str_eurotherm_slave, slave);
 }
 
+void Config::setExperimentator(const QString &name)
+{
+    settings.setValue(str_experimentator, name);
+}
+
 void Config::setHp34970Port(const QString &port)
 {
     settings.setValue(str_hp34970_port, port);
@@ -69,4 +123,39 @@ void Config::setMsdpPort(const QString &port)
 void Config::setPs6220Port(const QString &port)
 {
     settings.setValue(str_ps6220_port, port);
+}
+
+void Config::setResistivity_I(double I)
+{
+    settings.setValue(str_resistivity_i, I);
+}
+
+void Config::setSample_h(double h)
+{
+    settings.setValue(str_sample_h, h);
+}
+
+void Config::setSample_l1(double l)
+{
+    settings.setValue(str_sample_l1, l);
+}
+
+void Config::setSample_l2(double l)
+{
+    settings.setValue(str_sample_l2, l);
+}
+
+void Config::setSample_l3(double l)
+{
+    settings.setValue(str_sample_l3, l);
+}
+
+void Config::setSample_S(double S)
+{
+    settings.setValue(str_sample_S, S);
+}
+
+void Config::setSample_w(double w)
+{
+    settings.setValue(str_sample_w, w);
 }
--- Seebeck/config.h
+++ Seebeck/config.h
@@ -11,16 +11,32 @@ public:
     QString dataDir();
     QString eurothermPort();
     int eurothermSlave();
+    QString experimentator();
     QString hp34970Port();
     QString ps6220Port();
     QString msdpPort();
+    double resistivity_I();
+    double sample_h();
+    double sample_1();
+    double sample_l2();
+    double sample_l3();
+    double sample_S();
+    double sample_w();
 
     void setDataDir(const QString &dirName);
     void setEurothermPort(const QString &port);
     void setEurothermSlave(int slave);
+    void setExperimentator(const QString &name);
     void setHp34970Port(const QString &port);
     void setPs6220Port(const QString &port);
     void setMsdpPort(const QString &port);
+    void setResistivity_I(double I);
+    void setSample_h(double h);
+    void setSample_l1(double l);
+    void setSample_l2(double l);
+    void setSample_l3(double l);
+    void setSample_S(double S);
+    void setSample_w(double w);
 
 private:
     QSettings settings;
@@ -31,6 +47,14 @@ private:
     static const char str_hp34970_port[];
     static const char str_msdp_port[];
     static const char str_ps6220_port[];
+    static const char str_sample_l1[];
+    static const char str_sample_l2[];
+    static const char str_sample_l3[];
+    static const char str_sample_S[];
+    static const char str_sample_h[];
+    static const char str_resistivity_i[];
+    static const char str_sample_w[];
+    static const char str_experimentator[];
 };
 
 #endif // CONFIG_H
